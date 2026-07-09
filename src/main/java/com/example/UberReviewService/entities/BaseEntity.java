a44c8a6cd83438de06b24237bb303afe8b5e861e
Rishank Kashyap <167917876+Rishank-Kashyap@users.noreply.github.com>
2026-07-09 09:14:14 +0530

    Added BaseEntity class using MappedSuperclass

---
+++ src/main/java/com/example/UberReviewService/entities/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.example.UberReviewService.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+@Setter
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP) // this annotation tells spring about the format of Date object to be stored i.e. DATE, TIME or TIMESTAMP
+    @CreatedDate // this annotation tells spring to handle it for object creation
+    protected Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate // this annotation tells spring to handle it for object modification
+    protected Date updatedAt;
+}
--- src/main/java/com/example/UberReviewService/entities/Review.java
+++ src/main/java/com/example/UberReviewService/entities/Review.java
@@ -2,37 +2,22 @@ package com.example.UberReviewService.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.util.Date;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@EntityListeners(AuditingEntityListener.class)
 @Entity
 @Table(name = "bookingReview")
-public class Review {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
+public class Review extends BaseEntity {
     @Column(nullable = false)
     private String content;
 
     private Double rating;
 
-    @Column(nullable = false)
-    @Temporal(TemporalType.TIMESTAMP) // this annotation tells spring about the format of Date object to be stored i.e. DATE, TIME or TIMESTAMP
-    @CreatedDate // this annotation tells spring to handle it for object creation
-    private Date createdAt;
-
-    @Column(nullable = false)
-    @Temporal(TemporalType.TIMESTAMP)
-    @LastModifiedDate // this annotation tells spring to handle it for object modification
-    private Date updatedAt;
+    @Override
+    public String toString() {
+        return "Review:" + content + ", rating:" + rating + ", createdAt:" + this.createdAt;
+    }
 }
